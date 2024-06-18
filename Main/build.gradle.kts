bfceb871b6aae702d94c149ee4ec744d46aa1fe7
Nazar Machekhin <n.machekhin@ukma.edu.ua>
2024-06-19 00:30:02 +0300

    added custom task that ensures that file for scores exhists and it is empty. If it is not emty, it copies it as OldScore and creates new that is empty.

--- Main/build.gradle.kts
+++ Main/build.gradle.kts
@@ -24,6 +24,37 @@ tasks.named<JavaExec>("run") {
     standardInput = System.`in`
 }
 
+tasks.register("CheckSaveFilePresence") {
+    val path="../ScoreSaver/src/main/resources/Score.txt"
+    doLast {
+        val file = file(path)
+        if (file.exists())
+            println("Score file exists, check is successful.")
+        else
+            println("Error, cannot find score file!")
+    };
+}
+
+tasks.register("EnshureCleanScoreboard") {
+    val path="../ScoreSaver/src/main/resources/Score.txt"
+    val new_path="../ScoreSaver/src/main/resources/Old_Score.txt"
+    val file = file(path)
+    if(!file.exists()) {
+        file.writeText("")
+        println("Crated new empty score file")
+    }
+    else
+    {
+        val new_file=file(new_path)
+        if(new_file.exists()) new_file.delete()
+        println("Deleted previous old score")
+        file.renameTo(new_file)
+        file.writeText("")
+        println("Crated new empty score file")
+    }
+}
+
+
 tasks.test {
     useJUnitPlatform()
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ plugins {
     id("java")
 }
 
+
 group = "org.example"
-version = "1.0.0"
+version = "2.0.0"
 
 repositories {
     mavenCentral()
@@ -12,8 +13,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(project(":UICustom"))
+    implementation(project(":ScoreSaver"))
+    implementation(project(":Main"))
 }
-
 
 
 tasks.test {
